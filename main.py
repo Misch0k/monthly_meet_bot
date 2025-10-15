c5a02e2e8abc16bd0380e174a7e48d99523ac32a
Mikhail Shakhramanyan <misch0k@MacBook-Air-Mikhail.local>
2025-10-15 15:19:16 +0300

    monthly planning test over

--- main.py
+++ main.py
@@ -584,14 +584,14 @@ class MonthlyMeetBot:
         if hasattr(application, 'job_queue') and application.job_queue:
             application.job_queue.run_monthly(
                 self.monthly_planning,
-                when=time(hour=12, minute=15),
+                when=time(hour=5, minute=55),
                 day=15,
                 name="monthly_planning"
             )
             
             application.job_queue.run_daily(
                 self.check_and_send_pending_notifications,
-                time=time(hour=12, minute=16),
+                time=time(hour=6, minute=0),
                 name="daily_notification_check"
             )
             
